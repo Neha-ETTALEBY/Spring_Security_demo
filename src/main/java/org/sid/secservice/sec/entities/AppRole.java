ad1f81228de4dfa1ab798f74c6fd157dccebb549
@Neha-ETTALEBY <ettaleby.neha@gmail.com>
2024-11-02 22:05:49 +0100

    Added AppRole class

--- src/main/java/org/sid/secservice/sec/entities/AppRole.java
+++ src/main/java/org/sid/secservice/sec/entities/AppRole.java
@@ -1,4 +1,21 @@
 package org.sid.secservice.sec.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AppRole {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String roleName;
+
 }
