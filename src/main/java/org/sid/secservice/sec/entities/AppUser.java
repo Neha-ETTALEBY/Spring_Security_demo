d192a729d5afd59a4dae40e7aeb56b5136ed9a49
@Neha-ETTALEBY <ettaleby.neha@gmail.com>
2024-11-02 22:06:04 +0100

    Added AppUser.java class

--- src/main/java/org/sid/secservice/sec/entities/AppUser.java
+++ src/main/java/org/sid/secservice/sec/entities/AppUser.java
@@ -1,4 +1,20 @@
 package org.sid.secservice.sec.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Collection;
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor
+
 public class AppUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    @ManyToMany
+    private Collection<AppRole> appRoles;
 }
